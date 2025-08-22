978619b0fae93217ee58987e2af82f9459bcaa74
Neda <neda@Nedas-MacBook-Air.local>
2025-08-22 19:13:33 +0330

    tic tac toe  game with vanilla js

---
+++ script.js
@@ -0,0 +1,22 @@
+
+const cells = document.querySelectorAll(".cell");
+const playerStatus = document.querySelector(".game--status");
+const restart = document.querySelector(".game--restart");
+let currentPlayer = "X";
+let board = ["", "", "", "", "", "", "", "", ""];
+let isGameActive = true;
+playerStatus.innerText = "It's " + currentPlayer + "'s turn";
+cells.forEach((cell, index) => {
+    cell.addEventListener("click", () => {
+      if (board[index] === "" && isGameActive) {
+        board[index] = currentPlayer;  
+        cell.innerText = currentPlayer;
+        currentPlayer = currentPlayer === "X" ? "O" : "X";
+        playerStatus.innerText = "It's "+currentPlayer+"'s turn";
+     
+      }
+    });
+  });
+  
+  
+ 
